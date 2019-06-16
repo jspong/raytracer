bbfd9750da2662d581e616e3793e4ba5ebe380e0
John Spong <jspong@gmail.com>
2019-06-15 23:57:55 -0700

    Rename Pixel to Vec3 and implement operations

--- raytrace.hs
+++ raytrace.hs
@@ -1,6 +1,36 @@
-type Pixel = (Float, Float, Float)
-type Row = [Pixel]
+type Vec3 = (Float, Float, Float)
+type Row = [Vec3]
 type Image = [Row]
+
+add :: Vec3 -> Vec3 -> Vec3
+add (a1, b1, c1) (a2, b2, c2) = (a1+a2, b1+b2, c1+c2)
+
+negate :: Vec3 -> Vec3
+negate (a,b,c) = (-a, -b, -c)
+
+multiply :: Vec3 -> Vec3 -> Vec3
+multiply (a1, b1, c1) (a2, b2, c2) = (a1*a2, b1*b2, c1*c2)
+
+divide :: Vec3 -> Vec3 -> Vec3
+divide (a1, b1, c1) (a2, b2, c2) = (a1/a2, b1/b2, c1/c2)
+
+scale :: Vec3 -> Float -> Vec3
+scale (a, b, c) x = (a*x, b*x, c*x)
+
+dot :: Vec3 -> Vec3 -> Float
+dot (a1, b1, c1) (a2, b2, c2) = a1*a2 + b1*b2 + c1*c2
+
+cross :: Vec3 -> Vec3 -> Vec3
+cross (a1, b1, c1) (a2, b2, c2) = (b1*c2-c1*b2, c1*a2-a1*c2, a1*b2-a2*b1)
+
+squared_length :: Vec3 -> Float
+squared_length (a, b, c) = a*a + b*b + c*c
+
+len = sqrt . squared_length
+
+normalize :: Vec3 -> Vec3
+normalize (a, b, c) = (a / l, b / l, c / l) where l = len (a,b,c)
+
 
 convert :: Integer -> Integer -> Float
 convert x nx = 255.99 * fromIntegral x / fromIntegral nx
@@ -11,12 +41,12 @@ genImage nx ny = [
    | y <- [ny - yi + 1 | yi <- [1 .. ny]]
                  ]
 
-strPixel :: Pixel -> String
-strPixel (r, g, b) = show (floor r) ++ " " ++ show (floor g) ++ " " ++ show (floor b)
+strVec3 :: Vec3 -> String
+strVec3 (r, g, b) = show (floor r) ++ " " ++ show (floor g) ++ " " ++ show (floor b)
 
 strRow :: Row -> String
 strRow [] = ""
-strRow (p:ps) = strPixel p ++ "\n" ++ strRow ps
+strRow (p:ps) = strVec3 p ++ "\n" ++ strRow ps
 
 strImage :: Image -> String
 strImage [] = ""
