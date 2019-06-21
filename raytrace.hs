44310ea57e64995e4cb5594113f8aae58b149d00
John Spong <jspong@gmail.com>
2019-06-20 21:32:08 -0700

    Rearrange functions

--- raytrace.hs
+++ raytrace.hs
@@ -1,6 +1,8 @@
 import Data.Maybe
 import System.Random
 
+-- Vec3 operations
+
 data Vec3 = Vec3 { x :: Float, y :: Float, z :: Float } deriving (Show)
 type Image = [Vec3]
 
@@ -36,37 +38,8 @@ normalize :: Vec3 -> Vec3
 normalize v = let l = len(v)
               in Vec3 (x v / l) (y v / l) (z v / l)
 
-data Ray = Ray { origin :: Vec3
-               , direction :: Vec3 } deriving (Show)
-
-pointAt :: Ray -> Float -> Vec3
-pointAt r t = add (origin r) (scale (direction r) t)
-
 lerp :: Vec3 -> Vec3 -> Float -> Vec3
 lerp x y t = add (scale x (1-t)) (scale y t)
-
-hitColor :: StdGen -> (Maybe HitRecord) -> Ray -> [Hitable] -> Vec3
-hitColor _ Nothing r _ = lerp (Vec3 1.0 1.0 1.0) (Vec3 0.5 0.7 1.0) (0.5 * ((y (normalize (direction r))) + 1))
-hitColor g (Just rec) _ w = let (u, g') = randomInUnitSphere g
-                                target = add (add (position rec) (normal rec)) u
-                            in scale (color_ (Ray (position rec) (add target (negate3 (position rec)))) g') 0.5
-
-randomInUnitSphere :: StdGen -> (Vec3, StdGen)
-randomInUnitSphere g = let range = (-1.0, 1.0)
-                           (x, g') = randomR range g
-                           (y, g'') = randomR range g'
-                           (z, g''') = randomR range g''
-                           v = Vec3 x y z
-                       in if squared_length v < 1.0 then (v, g''') else randomInUnitSphere g'''
-
-color_ :: Ray -> StdGen -> Vec3
-color_ r g = let world = [(Sphere (Vec3 0.0 0.0 (-1.0)) 0.5), (Sphere (Vec3 0.0 (-100.5) (-1.0)) 100.0)]
-            in (hitColor g (getClosestHit world r 0.000001 10000000) r world)
-
-color :: Ray -> StdGen -> Vec3
-color r g = let v = color_ r g
-            in scale (Vec3 (sqrt $ x v) (sqrt $ y v) (sqrt $ z v)) 255.99
-
 
 average3_ :: [Vec3] -> Vec3 -> Int -> Vec3
 average3_ [] a n = scale a (1.0 / fromIntegral n)
@@ -75,6 +48,24 @@ average3_ (x:xs) a n = average3_ xs (add a x) (n+1)
 average3 :: [Vec3] -> Vec3
 average3 [] = Vec3 0.0 0.0 0.0
 average3 xs = average3_ xs (Vec3 0.0 0.0 0.0) 0
+
+-- Ray operations
+
+data Ray = Ray { origin :: Vec3
+               , direction :: Vec3 } deriving (Show)
+
+pointAt :: Ray -> Float -> Vec3
+pointAt r t = add (origin r) (scale (direction r) t)
+
+-- Collision Physics
+
+randomInUnitSphere :: StdGen -> (Vec3, StdGen)
+randomInUnitSphere g = let range = (-1.0, 1.0)
+                           (x, g') = randomR range g
+                           (y, g'') = randomR range g'
+                           (z, g''') = randomR range g''
+                           v = Vec3 x y z
+                       in if squared_length v < 1.0 then (v, g''') else randomInUnitSphere g'''
 
 data Hitable = Sphere { center :: Vec3 , radius :: Float }
 
@@ -115,14 +106,7 @@ getClosestHit (x:xs) r tMin tMax = let this = (hit x r tMin tMax)
                                    else let other = getClosestHit xs r tMin (closestTime tMax this)
                                         in closestHit this other
 
-data Camera = Camera { lower_left :: Vec3, horizontal :: Vec3, vertical :: Vec3, imgOrigin :: Vec3 }
-camera = Camera (Vec3 (-2.0) (-1.0) (-1.0)) (Vec3 4.0 0.0 0.0) (Vec3 0.0 2.0 0.0) (Vec3 0.0 0.0 0.0)
-
-getRay :: Camera -> Float -> Float -> Ray
-getRay c u v = Ray (imgOrigin c) (add (lower_left c) (add (scale (horizontal c) u) (add (scale (vertical c) v) (negate3 (imgOrigin c)))))
-
-rands :: StdGen -> [StdGen]
-rands g = let (g', g'') = split g in (g':rands g'')
+-- Image Generation
 
 coords :: StdGen -> Integer -> Integer -> Integer -> [(StdGen, Integer, Integer)]
 coords g x nx ny = if x == nx
@@ -146,6 +130,31 @@ strVec3 v = show (floor (x v)) ++ " " ++ show (floor (y v)) ++ " " ++ show (floo
 strImage :: Image -> String
 strImage [] = ""
 strImage (x:xs) = strVec3 x ++ "\n" ++ strImage xs
+
+-- Rendering Colors
+
+hitColor :: StdGen -> (Maybe HitRecord) -> Ray -> [Hitable] -> Vec3
+hitColor _ Nothing r _ = lerp (Vec3 1.0 1.0 1.0) (Vec3 0.5 0.7 1.0) (0.5 * ((y (normalize (direction r))) + 1))
+hitColor g (Just rec) _ w = let (u, g') = randomInUnitSphere g
+                                target = add (add (position rec) (normal rec)) u
+                            in scale (color_ (Ray (position rec) (add target (negate3 (position rec)))) g') 0.5
+
+color_ :: Ray -> StdGen -> Vec3
+color_ r g = let world = [(Sphere (Vec3 0.0 0.0 (-1.0)) 0.5), (Sphere (Vec3 0.0 (-100.5) (-1.0)) 100.0)]
+            in (hitColor g (getClosestHit world r 0.000001 10000000) r world)
+
+color :: Ray -> StdGen -> Vec3
+color r g = let v = color_ r g
+            in scale (Vec3 (sqrt $ x v) (sqrt $ y v) (sqrt $ z v)) 255.99
+
+data Camera = Camera { lower_left :: Vec3, horizontal :: Vec3, vertical :: Vec3, imgOrigin :: Vec3 }
+camera = Camera (Vec3 (-2.0) (-1.0) (-1.0)) (Vec3 4.0 0.0 0.0) (Vec3 0.0 2.0 0.0) (Vec3 0.0 0.0 0.0)
+
+getRay :: Camera -> Float -> Float -> Ray
+getRay c u v = Ray (imgOrigin c) (add (lower_left c) (add (scale (horizontal c) u) (add (scale (vertical c) v) (negate3 (imgOrigin c)))))
+
+rands :: StdGen -> [StdGen]
+rands g = let (g', g'') = split g in (g':rands g'')
 
 main = do {
    putStrLn "P3";
